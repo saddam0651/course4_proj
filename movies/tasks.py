a148c31b60fc35ef00e396f345d59ff776388266
Mohammad Haque <m.s.haque5178811@gmail.com>
2025-08-16 13:32:37 +0000

    Finish django signals

--- movies/tasks.py
+++ movies/tasks.py
@@ -1,6 +1,7 @@
 from celery import shared_task
 
 from movies import omdb_integration
+from django.core.mail import mail_admins
 
 
 @shared_task
@@ -8,3 +9,6 @@ def search_and_save(search):
     return omdb_integration.search_and_save(search)
 
 
+@shared_task
+def notify_of_new_search_term(search_term):
+    mail_admins("New Search Term", f"A new search term was used: '{search_term}'")
